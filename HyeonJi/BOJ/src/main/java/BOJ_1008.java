d60498301b488d7dcdefb4ab60c1641ac8aa83fd
DESKTOP-P581UF8\primo <a01086905329@gmail.com>
2023-05-23 23:44:30 +0900

    [현지] BOJ_1000, BOJ_1001, BOJ_1008

---
+++ HyeonJi/BOJ/src/main/java/BOJ_1008.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class BOJ_1008 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		double a, b;
+		a = sc.nextDouble();
+		b = sc.nextDouble();
+		System.out.println(a / b);
+	}
+
+}
