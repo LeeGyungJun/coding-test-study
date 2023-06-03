fbc10fd20802d1af96d0176e69c71c001f203126
DESKTOP-P581UF8\primo <a01086905329@gmail.com>
2023-06-03 19:08:02 +0900

    [현지] BOJ_1001 파일 내용 복구

--- HyeonJi/BOJ/src/main/java/BOJ_1001.java
+++ HyeonJi/BOJ/src/main/java/BOJ_1001.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 
 public class BOJ_1001 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		System.out.println(a - b);
 
 	}
 
